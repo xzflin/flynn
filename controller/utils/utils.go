540e29805a84937a0b4e46afcba1075853ce4074
Joseph Glanville <jpg@jpg.id.au>
2016-11-03 17:55:35 +1100

    controller/scheduler: Sync host sinks with controller
    Signed-off-by: Joseph Glanville <jpg@jpg.id.au>

--- controller/utils/utils.go
+++ controller/utils/utils.go
@@ -233,6 +233,9 @@ type HostClient interface {
 	ListJobs() (map[string]host.ActiveJob, error)
 	StreamEvents(id string, ch chan *host.Event) (stream.Stream, error)
 	GetStatus() (*host.HostStatus, error)
+	GetSinks() ([]*ct.Sink, error)
+	AddSink(*ct.Sink) error
+	RemoveSink(string) error
 }
 
 type ClusterClient interface {
@@ -255,6 +258,8 @@ type ControllerClient interface {
 	FormationListActive() ([]*ct.ExpandedFormation, error)
 	PutJob(*ct.Job) error
 	JobListActive() ([]*ct.Job, error)
+	StreamSinks(since *time.Time, ch chan *ct.Sink) (stream.Stream, error)
+	ListSinks() ([]*ct.Sink, error)
 }
 
 func ClusterClientWrapper(c *cluster.Client) clusterClientWrapper {
